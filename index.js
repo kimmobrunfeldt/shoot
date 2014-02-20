6e414ab61af586afa0aa37809b4bb7ae2edebad5
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-02-20 21:43:49 +0200

    Add resolution and help parameters

--- index.js
+++ index.js
@@ -1,119 +1,73 @@
+#!/usr/bin/env node
+
 var Q = require('q');
 var _ = require('lodash');
 var _s = require('underscore.string');
-
 var url = require('url');
 var request = require('request');
-var cheerio = require('cheerio');
 var pageres = require('pageres');
 var path = require('path');
+var Tarantula = require('tarantula');
+
 
 var sysargs = process.argv.slice(2);
 
 
-var MAX_DEPTH = 3;
-var LOWERCASE = false;
-var STAY_ON_BASE = true;
-
-
-var Spider = (function() {
-
-    var visited = [];
-    var initialUrl;
-
-    function findUrls(html) {
-        $ = cheerio.load(html);
-
-        var urls = [];
-        $('a').each(function() {
-            urls.push($(this).attr('href'));
-        });
-        return urls;
-    }
-
-    function processUrl(pageUrl, depth, cb) {
-        visited.push(fixUrlCase(pageUrl));
-        cb(pageUrl);
-        console.log(pageUrl, depth, isVisited(pageUrl));
-
-        if (depth > MAX_DEPTH) {
-            return;
-        }
-
-        request(pageUrl, function(error, response, body) {
-            console.log('request', pageUrl);
-            if (!error && _s.include(response.headers['content-type'], 'text/html') && response.statusCode == 200) {
-                var urls = findUrls(body);
-
-                var visitNext = _.filter(urls, function(u) {
-                    u = formatUrl(pageUrl, u);
-                    return u && !isVisited(u) && !isOutsideBase(u);
-                });
-
-                _.each(visitNext, function(u) {
-                    processUrl(formatUrl(pageUrl, u), depth + 1, cb);
-                });
-            }
-        });
-    }
-
-    function isVisited(pageUrl) {
-        return _.indexOf(visited, pageUrl) !== -1;
-    }
-
-    function isOutsideBase(pageUrl) {
-        var host = url.parse(pageUrl).host;
-        var initialHost = url.parse(initialUrl).host;
-        console.log(initialUrl, pageUrl);
-        console.log(initialHost, host);
-        return host !== initialHost;
-    }
-
-    function fixUrlCase(pageUrl) {
-        return LOWERCASE ? pageUrl.toLowerCase() : pageUrl;
-    }
-
-    function formatUrl(baseUrl, link) {
-        if (!_s.startsWith(link, 'http')) {
-            if (!_s.endsWith(baseUrl, '/')) baseUrl += '/';
-            if (_s.startsWith(link, '/')) link = link.slice(1);
-            return baseUrl + link;
-        }
-
-        return fixUrlCase(link);
-    }
-
-    return {
-        crawl: function(pageUrl, cb) {
-            initialUrl = pageUrl;
-            processUrl(pageUrl, 0, cb);
-        }
-    };
-})();
-
+function urlsInSameHost(url1, url2) {
+    var host1 = url.parse(url1).host;
+    var host2 = url.parse(url2).host;
+    return host1 === host2;
+}
 
 
 function main() {
-    if (!sysargs[0]) {
-        console.log("Usage: node index.js http://google.com");
+    if (!sysargs[0] || sysargs[0] == '-h' || sysargs[0] == '--help') {
+        console.log('Usage: node index.js <url> [resolution]');
+        console.log('Examples: ');
+        console.log('  node index.js http://google.com');
+        console.log('  node index.js http://google.com 800x600');
         process.exit(1);
     }
 
     var baseUrl = sysargs[0];
+    var resolution = sysargs[1] || '1366x768';
 
-    Spider.crawl(baseUrl, function(crawlUrl) {
-        var dirPath = path.join('shoot', url.parse(crawlUrl).pathname);
+
+    var brain = {
+        legs: 8,
+        shouldVisit: function(uri) {
+            return urlsInSameHost(baseUrl, uri.uri);
+        }
+    };
+
+    var tarantula = new Tarantula(brain);
+
+    tarantula.on('data', function(uri) {
+        var crawlUrl = uri.uri;
+        console.log('Processing', crawlUrl, '..');
+
+        var resolutionPath = path.join('shoot', resolution)
+        var dirPath = path.join(resolutionPath, url.parse(crawlUrl).pathname);
         var filePath = path.join(dirPath, 'index');
 
         pageres([{
             url: crawlUrl,
             filePath: filePath
-        }], ['1366x768'], function() {
-            console.log('Saved ', crawlUrl);
+        }], [resolution], function() {
+            console.log('Saved', crawlUrl, '->', filePath);
         });
     });
 
+    tarantula.on('done', function() {
+        console.log('Crawling done.');
+    });
 
+    tarantula.on('error', function(uri) {
+        var errorUrl = uri.uri;
+        console.log('Skipped', errorUrl);
+    });
+
+    tarantula.start([baseUrl]);
 }
 
 main();
