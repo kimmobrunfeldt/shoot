bfa03868316f7b675ed836180fe3c26df27f43d6
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-03-02 04:09:03 +0200

    Add mocha tests and travis conf

---
+++ test/test-utils.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+var utils = require("../utils");
+
+describe('utils', function() {
+    describe('rmDir', function() {
+        it('should remove given directory', function() {
+            assert.equal(1, 1, 'Dummy test to get travis working');
+        });
+    });
+});
