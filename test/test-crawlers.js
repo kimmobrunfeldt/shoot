eb768acf037c6a0ae8ce1c95344e121b0ce363da
Kimmo Brunfeldt <kimmo.brunfeldt@futurice.com>
2014-03-02 04:12:17 +0200

    Add dummy tests

---
+++ test/test-crawlers.js
@@ -0,0 +1,10 @@
+var assert = require("assert");
+var utils = require("../crawlers");
+
+describe('StaticCrawler', function() {
+    describe('start', function() {
+        it('should start crawling', function() {
+            assert.equal(1, 1, 'Dummy test to get travis working');
+        });
+    });
+});
